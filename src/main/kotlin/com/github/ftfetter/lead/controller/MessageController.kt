ae9128fd10c53d7b2306281f28059e2036586bcf
anujlucky007 <anujlucky007@gmail.com>
2020-06-19 12:59:57 +0530

    adding formpost functionality

--- src/main/kotlin/com/github/ftfetter/lead/controller/MessageController.kt
+++ src/main/kotlin/com/github/ftfetter/lead/controller/MessageController.kt
@@ -1,16 +1,17 @@
 package com.github.ftfetter.lead.controller
 
-import com.github.ftfetter.lead.model.WebMessage
 import com.github.ftfetter.lead.repository.MessageRepository
 import com.github.ftfetter.lead.service.MessageService
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import javax.validation.Valid
+import org.springframework.util.MultiValueMap
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ServerWebExchange
+import org.synchronoss.cloud.nio.multipart.MultipartUtils.getHeaders
+import reactor.core.Disposable
+import reactor.core.publisher.Mono
+import java.util.*
+
 
 @RestController
 @RequestMapping("/message")
@@ -19,8 +20,23 @@ class MessageController(val messageRepository: MessageRepository,val messageServ
     @GetMapping
     fun getAllTweets() = messageRepository.findAll()
 
-    @PostMapping
-    fun saveMessage(@Valid @RequestBody webMessage: WebMessage) = messageService.saveMessage(webMessage)
+    @RequestMapping(value = ["/whatsapp"], method = [RequestMethod.POST],consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
+    fun saveMessage(exchange: ServerWebExchange ): Mono<HashMap<String, Any>> {
+        val form = HashMap<String, Any>()
+        return  exchange.getFormData().map{
+             map ->
+            for ((key, value1) in map.entries) {
+                for (value in value1) {
+                    form[key] = value
+                }
+            }
+            println(form)
+            form
+        }.map{
+            it
+        }
+       // messageService.saveMessage(WebMessage("11", Contact("11","1","1"), Message("","","","")))
+    }
 
     @GetMapping("/{id}")
     fun getMessageByMobileNumber(@PathVariable("id") customerMobileNumber: String) = messageRepository.findById(customerMobileNumber)
